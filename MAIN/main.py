c47a9a0f7809b90e054b4654a75338fff7d443df
AFRA-TAJ <153506986+AFRA-TAJ@users.noreply.github.com>
2024-02-27 11:01:40 +0530

    main.py

--- MAIN/main.py
+++ MAIN/main.py
@@ -4,148 +4,103 @@
 # In[ ]:
 
 
-from ENTITY.customers import customers
-from ENTITY.products import products
-from ENTITY.orders import orders
-from ENTITY.cart import cart
-from ENTITY.order_items import order_items
-from DAO.createProduct import createProduct
-from DAO.createCustomer import createCustomer
-from DAO.deleteProduct import deleteProduct
-from DAO.deleteCustomer import deleteCustomer
-from DAO.addToCart import addToCart
-from DAO.removeFromCart import removeFromCart
-from DAO.getAllFromCart import getAllFromCart
-from DAO.getOrderByCustomer import getOrderByCustomer
-from EXCEPTION.CustomerNotFoundException import CustomerNotFoundException
-from EXCEPTION.ProductNotFoundException import ProductNotFoundException
-from EXCEPTION.OrderNotFoundException import OrderNotFoundException
-import unittest
-
-class EcomService:
-    def __init__(self):
-        self.createcustomer=createCustomer()
-        self.createproduct=createProduct()
-        self.deleteproduct=deleteProduct()
-        self.addtocart=addToCart()
-        self.getallfromcart=getAllFromCart()
-        self.getorderbycustomer=getOrderByCustomer()
-
-    def createCustomer(self):
-        customer_id=int(input("Enter Customer ID:"))
-        name=input("Enter Customer Name:")
-        email=input("Enter Customer Email:")
-        password=int(input("Enter Customer Password:"))
-
-        new_customer = customers(customer_id=customer_id,name=name,email=email,password=password)
-        
-        print(f"Customer ID: {new_customer.customer_id}")
-        print(f"Name: {new_customer.name}")
-        print(f"Email: {new_customer.email}")
-        print(f"Password:{new_customer.password}")
-        return True
-    
-    def createProduct(self):
-        product_id=int(input("Enter Product ID:"))
-        name=input("Enter product name: ")
-        price=int(input("Enter product price: "))
-        description=input("Enter prodcut description:")
-        stock_quantity=int(input("Enter product quantity: "))
-
-        new_product=products(product_id=product_id,name=name,price=price,description=description,stock_quantity=stock_quantity)
-
-        print(f"Product ID:{new_product.product_id}")
-        print(f"Name: {new_product.name}")
-        print(f"Price: ${new_product.price}")
-        print(f"Description:{new_product.description}")
-        print(f"Stock Quantity: {new_product.stock_quantity}")
-        return True
-
-    class customers:
-    def __init__(self, customer_id, name, email, password):
-        self.customer_id = customer_id
-        self.name = name
-        self.email = email
-        self.password = password
-
-    def deleteCustomer(self, customer_id_to_delete):
-        if self.customer_id == customer_id_to_delete:
-            print(f"Customer with ID {customer_id_to_delete} is deleted")
-            return True
-        else:
-            raise CustomerNotFoundException(f"Customer with ID {customer_id_to_delete} not found")
-class CustomerNotFoundException(Exception):
-    pass
-customer_instance = customers(customer_id=1, name="Yash", email="yash@example.com", password=1234)
-try:
-    result = customer_instance.deleteCustomer(1)
-    print("Customer deletion was successful.")
-except CustomerNotFoundException as e:
-    print(f"Deletion failed: {e}")
-
-
-    def addToCart(self,customer,product,quantity):
-        print("Added !")
-        return True
-    
-    def getAllFromCart(self,customer):
-        products_cart= [products(product_id=1,name="mobile",price=10000,description="long battery", stock_quantity=100),
-                   products(product_id=2,name="tv",price=80000,description="led screen", stock_quantity=50),
-                   products(product_id=3,name="laptop",price=50000,description="360 foldable", stock_quantity=150),
-                   products(product_id=4,name="bluetooth",price=5000,description="wireless", stock_quantity=250)]
-        return products_cart
-            
-    def getOrdersByCustomer(customerid):
-        orders ={1:{"product":"mobile","quantity":100},2: {"product":"tv","quantity":50}, 3: {"product":"laptop","quantity":70},
-                  4: {"product":"bluetooth","quantity":150}}
-
-        customer_orders = [{"product": order["product"], "quantity": order["quantity"]}
-        for order_id, order in orders.items()
-        if order.get("customerid")==customerid]
-
-        return customer_orders 
-
 class EcomApp:
     def __init__(self):
-        self.ecom_service = EcomService()
+        self.opr = OrderProcessorRepository()
 
-    def display_menu(self):
-        print("E-commerce Application Menu:")
-        print("1. Create Customer")
-        print("2. Create Product")
-        print("3. Delete Product")
-        print("4. Add to Cart")
-        print("5. View Cart")
-        print("6. View Customer Order")
-        print("7. Exit")
-
-    def run(self):
+    def main(self):
         while True:
-            self.display_menu()
-            choice = input("Enter your choice (1-7): ")
+            print('\nECOM APP')
+            print('SELECT 1 to Register Customer')
+            print('SELECT 2 to Create Product')
+            print('SELECT 3 to Delete Product')
+            print('SELECT 4 to Add to cart')
+            print('SELECT 5 to View cart')
+            print('SELECT 6 to Place order')
+            print('SELECT 7 to View Customer Order')
+            print('SELECT 8 to EXIT')
+            choice = int(input())
 
-            if choice == "1":
-                self.ecom_service.createCustomer()
-            elif choice == "2":
-                self.ecom_service.createProduct()
-            elif choice == "3":
-                self.ecom_service.deleteProduct()
-            elif choice == "4":
-                self.ecom_service.add_to_cart()
-            elif choice == "5":
-                self.ecom_service.view_cart()
-            elif choice == "6":
-                self.ecom_service.view_customer_order()
-            elif choice == "7":
-                print("Exiting the application.")
+            if choice == 1:
+                customer_id = int(input('Enter Customer ID: '))
+                cname = input('Enter Name: ')
+                email = input('Enter Email: ')
+                password = input('Enter Password: ')
+                customer = Customers(customer_id=customer_id, cname=cname, email=email, password=password)
+                self.opr.create_customer(customer)
+
+            elif choice == 2:
+                product_id = int(input('Enter Product id: '))
+                pname = input('Enter Product Name: ')
+                price = float(input('Enter Product Price: '))
+                description = input('Enter Product Description: ')
+                stock_quantity = int(input('Enter the Stock Quantity: '))
+                product = Products(product_id = product_id, pname=pname, price=price, description=description, stock_quantity=stock_quantity)
+                self.opr.create_product(product)
+
+            elif choice == 3:
+                product_id = int(input('\nEnter the Product ID: '))
+                self.opr.delete_product(product_id)
+
+            elif choice == 4:
+                cart_id = int(input('\nEnter cart id:'))
+                customer_id = int(input('Enter the Customer ID: '))
+                product_id = int(input('Enter the Product ID: '))
+                quantity = int(input('Enter the Product Quantity: '))
+                product = Products(product_id=product_id, pname="", price=0.0, description="", stock_quantity=0)
+                cart = Cart(cart_id=cart_id, customer_id=customer_id, product_id=product_id, quantity=quantity)
+                self.opr.add_to_cart(cart, product, quantity)
+
+            elif choice == 5:
+                customer_id = int(input('\nEnter the Customer ID: '))
+                products_in_cart = self.opr.get_all_from_cart(customer_id)
+                if not products_in_cart:
+                    print('\nYour Cart is Empty...')
+                else:
+                    print('\nFollowing are the Cart Items:')
+                    for item in products_in_cart:
+                        product_id, quantity = item['product_id'], item['quantity']
+                        print(f'Product ID: {product_id} - {quantity}')
+
+
+
+            elif choice == 6:
+                try:
+                    customer_id = int(input('\nEnter the Customer ID: '))
+                    products_in_cart = self.opr.get_all_from_cart(customer_id)
+                    if not products_in_cart:
+                        print('\nYour Cart is Empty...')
+                    else:
+                        try:
+                            order_id = int(input('\nEnter order ID: '))
+                            shipping_address = input('\nEnter the Shipping Address: ')
+                            # Proceed with order placement logic
+                            order_items = [(item['product_id'], item['quantity']) for item in products_in_cart]
+                            # Call the place_order method
+                            success = self.opr.place_order(customer_id, order_id, order_items)
+                            if success:
+                                print(f'Order {order_id} placed successfully.')
+                            else:
+                                print('Failed to place the order. Please try again.')
+                        except ValueError:
+                            print('Invalid input for order ID. Please enter a valid integer.')
+                except ValueError:
+                    print('Invalid input for Customer ID. Please enter a valid integer.')
+
+            elif choice == 7:
+                customer_id = int(input('\nEnter the Customer ID: '))
+                self.opr.get_orders_by_customer(customer_id)
+
+            elif choice == 8:
+                print('\nThank You !')
                 break
+
             else:
-                print("Invalid choice. Please enter a number between 1 and 7.")
+                print('\nInvalid Choice!')
 
 
 if __name__ == "__main__":
-    ecom_app = EcomApp()
-    ecom_app.run()
+    EcomApp().main()
 
 
 # In[ ]:

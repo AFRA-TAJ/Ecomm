6d649d911805c0f7ed8477ec56795d582694b177
AFRA-TAJ <153506986+AFRA-TAJ@users.noreply.github.com>
2024-02-27 11:08:41 +0530

    DBPropertyUtil.py

---
+++ UTIL/DBPropertyUtil.py
@@ -0,0 +1,8 @@
+class PropertyUtil:
+    @staticmethod
+    def getPropertyString():
+        host = 'localhost'
+        user = 'root'
+        password = 'Af#ra_02!'
+        database = 'Ecomm'
+        return host, user, password, database

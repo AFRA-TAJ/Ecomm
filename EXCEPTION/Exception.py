bf950dd285fe6beb169d5bd9c493ac5d4f17d644
AFRA-TAJ <153506986+AFRA-TAJ@users.noreply.github.com>
2024-02-27 11:00:49 +0530

    Exception.py

--- EXCEPTION/Exception.py
+++ EXCEPTION/Exception.py
@@ -8,14 +8,15 @@ class CustomerNotFoundException(Exception):
     def __init__(self, message="Invalid Customer ID"):
         self.message = message
         super().__init__(self.message)
-        
+
+
 class ProductNotFoundException(Exception):
     def __init__(self, message="Invalid Product ID"):
         self.message = message
         super().__init__(self.message)
 
+
 class OrderNotFoundException(Exception):
     def __init__(self, message="Invalid Order ID"):
         self.message = message
         super().__init__(self.message)
-

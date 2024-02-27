d09175dd2be13d16a567a1280a34c56b7c1a9765
AFRA-TAJ <153506986+AFRA-TAJ@users.noreply.github.com>
2024-02-27 11:11:02 +0530

    OrderRepo.py

--- DAO/addToCart.py
+++ DAO/addToCart.py
@@ -4,52 +4,206 @@
 # In[1]:
 
 
-class customers:
-    def __init__(self,customer_id,name,email,password):
-        self.customer_id=customer_id
-        self.name=name
-        self.email=email
-        self.password=password
-class products:
-    def __init__(self,product_id,name,price,description,stock_quantity):
-        self.product_id=product_id
-        self.name=name
-        self.price=price
-        self.description=description
-        self.stock_quantity=stock_quantity
-class cart:
-    def __init__(self,cart_id,customer_id,product_id,quantity):
-        self.cart_id=cart_id
-        self.customer_id=customer_id
-        self.product_id=product_id
-        self.quantity=quantity
-        
-    def addToCart(self,customer,product,quantity):
-        print("Added !")
+import mysql.connector as sql
+from UTIL.DBConnUtil import DbConnect
+from EXCEPTION.Exceptions import ProductNotFoundException, CustomerNotFoundException, OrderNotFoundException
+
+class OrderProcessorRepository(DbConnect):
+    def __init__(self):
+        self.db_connection = DbConnect()
+
+    def create_product(self, product):
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''INSERT INTO products (product_id, pname, price, description, stock_quantity)
+                    VALUES ('{product.product_id}', '{product.pname}', '{product.price}', '{product.description}', '{product.stock_quantity}')'''
+            )
+            print(f"Product with {product.product_id} created successfully.")
+            self.db_connection.conn.commit()
+            return True
+        except Exception as e:
+            print(e)
+            raise ProductNotFoundException("Failed to create product.")
+        finally:
+            self.db_connection.close()
+
+    def delete_product(self, product_id):
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(f'''Delete From products Where product_id = {product_id}''')
+            print(f'\nDeleted Product with ID - {product_id} from the database.')
+            self.db_connection.conn.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            self.db_connection.close()
+
+    def create_customer(self, customer):
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''INSERT INTO customers (customer_id, cname, email, password)
+                    VALUES ('{customer.customer_id}', '{customer.cname}', '{customer.email}', '{customer.password}')'''
+            )
+            print(f"Customer with {customer.customer_id} created successfully")
+            self.db_connection.conn.commit()
+            return True
+
+        except Exception as e:
+            print(e)
+            raise CustomerNotFoundException("Failed to create customer.")
+
+        finally:
+            self.db_connection.close()
+
+    def delete_customer(self, customer_id):
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(f'''DELETE FROM customers WHERE customer_id = {customer_id}''')
+            print("Customer deleted successfully.")
+            self.db_connection.conn.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            self.db_connection.close()
+
+    def add_to_cart(self, cart, product, quantity):
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''INSERT INTO cart (cart_id, customer_id, product_id, quantity)
+                       VALUES ({cart.cart_id}, {cart.customer_id}, {product.product_id}, {quantity})'''
+            )
+            print(f'\nItems added to the cart.')
+            self.db_connection.conn.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            self.db_connection.close()
+
+    def remove_from_cart(self, customer, product):
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''DELETE FROM cart WHERE customer_id = {customer.customer_id} 
+                AND product_id = {product.product_id}'''
+            )
+            self.db_connection.conn.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        finally:
+            self.db_connection.close()
+
+    def get_all_from_cart(self, customer_id):
+        products_in_cart = []
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''SELECT product_id, quantity
+                    FROM cart
+                    WHERE customer_id = {customer_id}'''
+            )
+            for row in self.db_connection.stmt:
+                product_id, quantity = row[0], row[1]
+                products_in_cart.append({'product_id': product_id, 'quantity': quantity})
+        except Exception as e:
+            print(e)
+        finally:
+            self.db_connection.close()
+            return products_in_cart
+
+    def place_order(self, customer_id, order_id, order_items):
+        total_price = 0
+
+        try:
+            for product_id, quantity, order_item_id in order_items:
+                self.db_connection.open()
+                self.db_connection.stmt.execute(
+                    f'SELECT price FROM products WHERE product_id = %s', (product_id,)
+                )
+                rows = self.db_connection.stmt.fetchall()
+
+                if rows:
+                    cost = rows[0][0]
+                    total_price += quantity * float(cost)
+                self.db_connection.close()
+        except Exception as e:
+            print(e)
+            return False
+
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''INSERT INTO orders (order_id, customer_id, order_date, total_price)
+                    VALUES (%s, %s, CURRENT_DATE(), %s)''',
+                (order_id, customer_id, total_price)
+            )
+            self.db_connection.conn.commit()
+        except Exception as e:
+            print(e)
+            return False
+
+        try:
+            for product_id, quantity, order_item_id in order_items:
+                self.db_connection.open()
+                self.db_connection.stmt.execute(
+                    f'''INSERT INTO order_items (order_item_id, order_id, product_id, quantity)
+                        VALUES (%s, %s, %s, %s)''',
+                    (order_item_id, order_id, product_id, quantity)
+                )
+                self.db_connection.conn.commit()
+
+                # Updating stock quantity
+                self.db_connection.stmt.execute(
+                    f'''UPDATE products SET stockQuantity = stockQuantity - %s
+                        WHERE product_id = %s''',
+                    (quantity, product_id)
+                )
+                self.db_connection.conn.commit()
+                self.db_connection.close()
+        except Exception as e:
+            print(e)
+            return False
+
+        print(f'\nOrder Placed Successfully..\nYour Order ID is {order_id}.')
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(
+                f'''DELETE FROM cart WHERE customer_id = %s''', (customer_id,)
+            )
+            self.db_connection.conn.commit()
+        except Exception as e:
+            print(e)
+            return False
+        finally:
+            self.db_connection.close()
+
         return True
 
-customer_id = int(input("Enter Customer ID: "))
-name = input("Enter customer name: ")
-email = input("Enter customer email: ")
-password=int(input("Enter customer password: "))
-customer = customers(customer_id,name,email,password)
+    def get_orders_by_customer(self, customer_id):
+        orders = []
+        try:
+            self.db_connection.open()
+            self.db_connection.stmt.execute(f'''SELECT *
+                                 FROM orders
+                                 WHERE customer_id = {customer_id}''')
 
-product_id = int(input("Enter Product ID: "))
-name = input("Enter product name: ")
-price =int(input("Enter product price: "))
-description=input("Enter Product Description: ")
-stock_quantity = int(input("Enter product stock quantity: "))
-product = products(product_id,name,price,description,stock_quantity)
+            orders = self.db_connection.stmt.fetchall()
 
-quantity=int(input("Enter quantity to add to the cart: "))
-cart=cart(cart_id=4, customer_id=customer_id, product_id=product_id, quantity=quantity)
+            print('\nThese are the Orders placed:')
+            for order in orders:
+                print(order)
 
-result=cart.addToCart(customer, product, quantity)
+        except Exception as e:
+            print(e)
+        finally:
+            self.db_connection.close()
 
-if result:
-    print("Product added to the cart successfully.")
-else:
-    print("Failed to add the product to the cart.")
+            if not orders:
+                print('None')
 
 
 # In[ ]:
